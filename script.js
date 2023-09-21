2dc0a33000bfd085475527c1a85ccb88fd34089f
Sarvesh Naik <naiksarvesh463@gmail.com>
2023-09-21 18:29:28 +0530

    Added a js file

---
+++ script.js
@@ -0,0 +1,37 @@
+const question1 = {
+  question: "What is your what?",
+  category: "Anxity",
+  a: ["Option", 5],
+  b: ["Option", 10],
+  c: ["Option", 15],
+  d: ["Option", 20],
+  maxPoints: 20
+};
+
+const question2 = {
+  question: "Since when are you suffering this?",
+  category: "Depression",
+  a: ["more than 12 months", 25],
+  b: ["6 moneths", 20],
+  c: ["3 monthes", 15],
+  d: ["less than 2 months", 0],
+  maxPoints: 25
+};
+
+const question3 = {
+  question: "What is your what?",
+  category: "Stress",
+  points: 15,
+};
+
+const question4 = {
+  question: "What is your what?",
+  category: "Schizophrenia",
+  points: 10,
+};
+
+const question5 = {
+  question: "What is your what?",
+  category: "Schizophrenia",
+  points: 20,
+};
